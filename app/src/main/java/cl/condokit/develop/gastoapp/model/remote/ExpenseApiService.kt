c7788ddca6987ec38bde3e9ebc7f8a0a243ad477
bryansotoa <b.soto.astudillo@gmail.com>
2025-10-12 23:40:48 -0300

    Revert "Guia Base de Gastos"
    This reverts commit 49fe3d6d6d1f5f6dc840a980eec6c3e38372259a.

--- app/src/main/java/cl/condokit/develop/gastoapp/model/remote/ExpenseApiService.kt
+++
@@ -1,9 +0,0 @@
-package cl.condokit.develop.gastoapp.model.remote
-
-// Esta interfaz simula un punto para futuras llamadas a APIs REST.
-// Puedes implementar Retrofit aquí si luego sincronizas los gastos con un servidor.
-
-interface ExpenseApiService {
-    // suspend fun obtenerGastosRemotos(): List<ExpenseDto>
-    // suspend fun enviarGasto(gasto: ExpenseDto)
-}
